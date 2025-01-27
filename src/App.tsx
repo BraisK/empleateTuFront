053421db9e9b35edf45283374772336858c8390e
BraisK <braiscasares26@gmail.com>
2025-01-27 13:09:16 +0100

    Login

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Profile from './pages/Profile'
+import UserList from './pages/UserList'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar/>
+        <div className='container mx-auto px-8 py-30'>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/userList' element={<UserList/>}/>
+        </Routes>
+        </div>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
